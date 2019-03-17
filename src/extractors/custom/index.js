112602743a88ae7a3bf3fd32795672ddb3e21e8c
trix180 <danilo.bonardi@shinyfrog.net>
2019-03-17 16:56:19 +0100

    extractor for withings.com

--- src/extractors/custom/index.js
+++ src/extractors/custom/index.js
@@ -110,3 +110,4 @@ export * from './www.nikonpassion.com';
 export * from './www.process.st';
 export * from './mp.weixin.qq.com';
 export * from './www.enchantingmarketing.com';
+export * from './www.withings.com';
