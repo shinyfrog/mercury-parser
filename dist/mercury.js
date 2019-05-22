01e63d3bb352f2c4664a19608704194b0278ba1e
trix180 <danilo.bonardi@shinyfrog.net>
2019-05-22 15:52:02 +0200

    new build

--- dist/mercury.js
+++ dist/mercury.js
@@ -8435,7 +8435,7 @@ var WwwThehungrychookComExtractor = {
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
     transforms: {
-      'div': 'p'
+      div: 'p'
     },
     // Is there anything that is in the result that shouldn't be?
     // The clean selectors will remove anything that matches from
@@ -8493,6 +8493,69 @@ var WwwSonimaComExtractor = {
   },
   content: {
     selectors: ['.post__inner .article-wrap:first-child section.article-body'],
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {},
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: []
+  }
+};
+
+var BlogYidengxuetangComExtractor = {
+  domain: 'blog.yidengxuetang.com',
+  title: {
+    selectors: ['article.post header h1']
+  },
+  author: {
+    selectors: [['meta[name="author"]', 'value']]
+  },
+  date_published: {
+    selectors: [['meta[name="article:published_time"]', 'value']]
+  },
+  dek: {
+    selectors: [// enter selectors
+    ]
+  },
+  lead_image_url: {
+    selectors: [// enter selectors
+    ]
+  },
+  content: {
+    selectors: ['article.post div.post-content'],
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {},
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: ['.post-archive', '.post-meta']
+  }
+};
+
+var JuejinImExtractor = {
+  domain: 'juejin.im',
+  title: {
+    selectors: ['.main-area article h1']
+  },
+  author: {
+    selectors: [// enter author selectors
+    ]
+  },
+  date_published: {
+    selectors: [['.main-area article time.time', 'datetime']]
+  },
+  dek: {
+    selectors: [// enter selectors
+    ]
+  },
+  lead_image_url: {
+    selectors: [// enter selectors
+    ]
+  },
+  content: {
+    selectors: ['.main-area article .article-content'],
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
     transforms: {},
@@ -8727,7 +8790,9 @@ var CustomExtractors = /*#__PURE__*/Object.freeze({
   WwwNzzChExtractor: WwwNzzChExtractor,
   WwwThehungrychookComExtractor: WwwThehungrychookComExtractor,
   WwwThetalkoComExtractor: WwwThetalkoComExtractor,
-  WwwSonimaComExtractor: WwwSonimaComExtractor
+  WwwSonimaComExtractor: WwwSonimaComExtractor,
+  BlogYidengxuetangComExtractor: BlogYidengxuetangComExtractor,
+  JuejinImExtractor: JuejinImExtractor
 });
 
 var Extractors = _Object$keys(CustomExtractors).reduce(function (acc, key) {
