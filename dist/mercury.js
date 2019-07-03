0cc5a5b3bcc0318a42d9d05ca160af94f8f51577
trix180 <danilo.bonardi@shinyfrog.net>
2019-07-03 10:25:20 +0100

    dist rebuild

--- dist/mercury.js
+++ dist/mercury.js
@@ -9148,6 +9148,37 @@ var PitchforkComExtractor = {
   }
 };
 
+var MnewsJoinsComExtractor = {
+  domain: 'mnews.joins.com',
+  title: {
+    selectors: [['meta[name="og:title"]', 'value']]
+  },
+  author: {
+    selectors: [// enter author selectors
+    ]
+  },
+  date_published: {
+    selectors: [['meta[name="article:published_time"]', 'value']]
+  },
+  dek: {
+    selectors: [// enter selectors
+    ]
+  },
+  lead_image_url: {
+    selectors: [['meta[name="og:image"]', 'value']]
+  },
+  content: {
+    selectors: ['#article_body'],
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {},
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: []
+  }
+};
+
 
 
 var CustomExtractors = /*#__PURE__*/Object.freeze({
@@ -9393,7 +9424,8 @@ var CustomExtractors = /*#__PURE__*/Object.freeze({
   WwwFujixpassionComExtractor: WwwFujixpassionComExtractor,
   XelzInfoExtractor: XelzInfoExtractor,
   WwwPhoronixComExtractor: WwwPhoronixComExtractor,
-  PitchforkComExtractor: PitchforkComExtractor
+  PitchforkComExtractor: PitchforkComExtractor,
+  MnewsJoinsComExtractor: MnewsJoinsComExtractor
 });
 
 var Extractors = _Object$keys(CustomExtractors).reduce(function (acc, key) {
