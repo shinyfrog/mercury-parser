43fb2535f305f0ecb25c8af52cfce5758b14bdf4
trix180 <danilo.bonardi@shinyfrog.net>
2020-03-13 10:58:58 +0100

    build update

--- dist/mercury.js
+++ dist/mercury.js
@@ -5791,7 +5791,7 @@ var FortelabsCoExtractor = {
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
     transforms: {
-      'div': 'span'
+      div: 'span'
     },
     // Is there anything that is in the result that shouldn't be?
     // The clean selectors will remove anything that matches from
