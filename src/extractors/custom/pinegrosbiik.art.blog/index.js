a17d216c17e9889129d20f2b3f8b0192b75c977f
trix180 <danilo.bonardi@shinyfrog.net>
2020-05-05 14:56:51 +0200

    2 new parsers

---
+++ src/extractors/custom/pinegrosbiik.art.blog/index.js
@@ -0,0 +1,38 @@
+export const PinegrosbiikArtBlogExtractor = {
+  domain: 'pinegrosbiik.art.blog',
+
+  title: {
+    selectors: [['meta[name="og:title"]', 'value']],
+  },
+
+  author: {
+    selectors: [],
+  },
+
+  date_published: {
+    selectors: [['meta[name="article:published_time"]', 'value']],
+  },
+
+  dek: {
+    selectors: [
+      // enter selectors
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [['meta[name="og:image"]', 'value']],
+  },
+
+  content: {
+    selectors: ['.entry-content'],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {},
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [],
+  },
+};
