325eb99da94805956aae1454b45684a48526ebe1
trix180 <danilo.bonardi@shinyfrog.net>
2020-11-17 13:45:51 +0100

    fix

---
+++ src/extractors/custom/www.blog.google/index.js
@@ -0,0 +1,40 @@
+export const WwwBlogGoogleExtractor = {
+  domain: 'www.blog.google',
+
+  title: {
+    selectors: [['meta[name="og:title"]', 'value']],
+  },
+
+  author: {
+    selectors: [
+      // enter author selectors
+    ],
+  },
+
+  date_published: {
+    selectors: [['meta[name="article:published_time"]', 'value']],
+  },
+
+  dek: {
+    selectors: [
+      // enter selectors
+    ],
+  },
+
+  lead_image_url: {
+    selectors: [['meta[name="og:image"]', 'value']],
+  },
+
+  content: {
+    selectors: ['div.uni-content'],
+
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {},
+
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: [],
+  },
+};
