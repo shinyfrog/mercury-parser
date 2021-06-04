983d893c03e2030d906e95a3ec56f5770222922d
trix180 <danilo.bonardi@shinyfrog.net>
2021-06-04 12:35:39 +0200

    dist update

--- dist/mercury.js
+++ dist/mercury.js
@@ -6230,12 +6230,12 @@ var NewsYcombinatorComExtractor = {
     // Is there anything in the content you selected that needs transformed
     // before it's consumable content? E.g., unusual lazy loaded images
     transforms: {
-      'div': 'span',
+      div: 'span',
       '.fatitem': 'p',
       '.comment-tree': 'p',
-      'table': 'span',
-      'tr': 'span',
-      'td': 'span'
+      table: 'span',
+      tr: 'span',
+      td: 'span'
     },
     // Is there anything that is in the result that shouldn't be?
     // The clean selectors will remove anything that matches from
