3e43adc79115cd3db745684ea6673b3a2fea6410
Danilo Bonardi <danilo.bonardi@shinyfrog.net>
2024-02-21 16:55:49 +0100

    new medium parser

--- src/extractors/custom/medium.com/index.test.js
+++ src/extractors/custom/medium.com/index.test.js
@@ -8,49 +8,56 @@ import { excerptContent } from 'utils/text';
 
 const fs = require('fs');
 
-describe('MediumExtractor', () => {
+describe('MediumComExtractor', () => {
   describe('initial test case', () => {
     let result;
     let url;
     beforeAll(() => {
       url =
-        'https://medium.com/the-wtf-economy/wtf-whats-the-future-e52ab9515573#.ilwrgwsks';
-      const html = fs.readFileSync('./fixtures/medium.com/1477523363921.html');
+        'https://medium.com/the-mission/elon-musks-3-step-first-principles-thinking-how-to-think-and-solve-difficult-problems-like-a-ba1e73a9f6c0';
+      const html = fs.readFileSync('./fixtures/medium.com/1708529277462.html');
       result = Mercury.parse(url, { html, fallback: false });
     });
 
-    it('is selected properly', async () => {
-      // To pass this test, rename your extractor in
-      // ./src/extractors/custom/medium.com/index.js
-      // (e.g., CustomExtractor => NYTimesExtractor)
-      // then add your new extractor to
-      // src/extractors/all.js
+    it('is selected properly', () => {
+      // This test should be passing by default.
+      // It sanity checks that the correct parser
+      // is being selected for URLs from this domain
       const extractor = getExtractor(url);
       assert.equal(extractor.domain, URL.parse(url).hostname);
     });
 
     it('returns the title', async () => {
+      // To pass this test, fill out the title selector
+      // in ./src/extractors/custom/medium.com/index.js.
       const { title } = await result;
 
-      assert.equal(title, 'WTF? What’s The Future?');
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(
+        title,
+        `Elon Musks’ “3-Step” First Principles Thinking: How to Think and Solve Difficult Problems Like a Genius`
+      );
     });
 
     it('returns the author', async () => {
+      // To pass this test, fill out the author selector
+      // in ./src/extractors/custom/medium.com/index.js.
       const { author } = await result;
 
-      assert.equal(author, "Tim O'Reilly");
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(author, null);
     });
 
     it('returns the date_published', async () => {
+      // To pass this test, fill out the date_published selector
+      // in ./src/extractors/custom/medium.com/index.js.
       const { date_published } = await result;
 
-      assert.equal(date_published, '2016-10-19T14:30:56.529Z');
-    });
-
-    it('returns the dek', async () => {
-      const { dek } = await result;
-
-      assert.equal(dek, null);
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(date_published, null);
     });
 
     it('returns the lead_image_url', async () => {
@@ -62,11 +69,25 @@ describe('MediumExtractor', () => {
       // the article.
       assert.equal(
         lead_image_url,
-        'https://miro.medium.com/max/540/1*3Gzaug9mRc8vvx1cuQWkog.png'
+        `https://miro.medium.com/v2/resize:fit:1080/1*HxEAnRcpkePVCcglWyKgoQ.png`
       );
     });
 
+    it('returns the dek', async () => {
+      // To pass this test, fill out the dek selector
+      // in ./src/extractors/custom/medium.com/index.js.
+      const { dek } = await result;
+
+      // Update these values with the expected values from
+      // the article.
+      assert.equal(dek, null);
+    });
+
     it('returns the content', async () => {
+      // To pass this test, fill out the content selector
+      // in ./src/extractors/custom/medium.com/index.js.
+      // You may also want to make use of the clean and transform
+      // options.
       const { content } = await result;
 
       const $ = cheerio.load(content || '');
@@ -78,33 +99,11 @@ describe('MediumExtractor', () => {
         13
       );
 
+      // Update these values with the expected values from
+      // the article.
       assert.equal(
         first13,
-        'Last Thursday, I had the honor to be one of the warmup acts'
-      );
-    });
-  });
-
-  describe('works with another url', () => {
-    let result;
-    let url;
-    beforeAll(() => {
-      url =
-        'https://medium.com/@JakobUlbrich/flag-attributes-in-android-how-to-use-them-ac4ec8aee7d1#.h949wjmyw';
-      const html = fs.readFileSync('./fixtures/medium.com/1485902752952.html');
-      result = Mercury.parse(url, { html, fallback: false });
-    });
-
-    it('returns the content', async () => {
-      const { content } = await result;
-
-      const $ = cheerio.load(content || '');
-
-      const first13 = excerptContent($.text(), 13);
-
-      assert.equal(
-        first13,
-        'I’m sure you have seen something like the following line very often while'
+        'By the age of 46 years old, Elon Musk has innovated and built'
       );
     });
   });
